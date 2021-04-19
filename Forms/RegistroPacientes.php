95ce3dea4b8d25d05360aa3a1a247ebe6843ec28
angel6143 <66202595+angel6143@users.noreply.github.com>
2021-04-19 00:53:34 -0500

    Insert

--- Forms/RegistroPacientes.php
+++ Forms/RegistroPacientes.php
@@ -1,6 +1,36 @@
 <?php
-    $menu = file_get_contents("RegistroPacientes.html");
-    $menuz = file_get_contents("../Layout/Encabezado.html");
-    $menu = preg_replace('/--menu--/',$menuz,$menu);
-    echo $menu
+require_once "../config/db.php";
+
+
+if(isset($_POST))
+{
+    if(isset($_POST["accion"]) =="guardar"){
+        echo InsertPaciente();
+    }else{
+        $menu = file_get_contents("RegistroPacientes.html");
+        $menuz = file_get_contents("../Layout/Encabezado.html");
+        $menu = preg_replace('/--menu--/',$menuz,$menu);
+        $parametros = preg_replace('/--acciones--/', '', $menu);
+        echo $menu;
+    }
+}
+function InsertPaciente()
+{
+    global $mysqli;
+    $accion="";
+    
+    $nombre = $_POST["nombre"];
+    $apellido =  $_POST["apellido"];
+    $fechanacimiento =  $_POST["fecha"];
+    $direccion =  $_POST["direccion"];
+    $dui =  $_POST["dui"];
+    $nit =  $_POST["nit"];
+    
+    $insert = mysqli_query($mysqli, "CALL sp_insertpacientes('$nombre','$apellido',$fechanacimiento,'$direccion','$dui','$nit')") or die(mysqli_error($mysqli));
+    while($result=mysqli_fetch_assoc($insert)){
+        $arr[]=$result;
+    }            
+    return json_encode($arr);
+}
+
 ?>
--- config/db.php
+++ config/db.php
@@ -4,13 +4,10 @@
     if ($mysqli->connect_errno) {
         echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
-    echo $mysqli->host_info . "\n";
-
     $mysqli = new mysqli("MYSQL5038.site4now.net", "a5e2c6_clinica", "clinica2021", "db_a5e2c6_clinica", 3306);
     if ($mysqli->connect_errno) {
         echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
 
-    echo $mysqli->host_info . "\n";
 
 ?>
